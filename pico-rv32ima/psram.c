620e5ea334556984a19e542a175299cef447e157
tvlad1234 <vlad.tomoiaga@gmail.com>
2023-04-11 17:41:04 +0300

    use sw bitbanged spi (faster than PIO it seems)

--- pico-rv32ima/psram.c
+++ pico-rv32ima/psram.c
@@ -2,11 +2,8 @@
 
 #include "f_util.h"
 #include "ff.h"
-#include "hw_config.h"
 
-#include "pio_spi.h"
 #include "rv32_config.h"
-#include "cdc_console.h"
 #include "psram.h"
 
 #define PSRAM_CMD_RES_EN 0x66
@@ -19,19 +16,52 @@
 
 #define RAM_HALF (EMULATOR_RAM_MB * 512 * 1024)
 
-pio_spi_inst_t pioSpi = {
-    .pio = pio0,
-    .sm = 0,
-    .cs_pin = PICO_DEFAULT_SPI_CSN_PIN};
-
 #define selectPsramChip(c) gpio_put(c, false)
 #define deSelectPsramChip(c) gpio_put(c, true)
+
+#define spi_set_mosi(value) gpio_put(PSRAM_SPI_PIN_TX, value)
+#define spi_read_miso() gpio_get(PSRAM_SPI_PIN_RX)
+
+#define spi_pulse_sck()                \
+    {                                  \
+        asm("nop");                    \
+        gpio_put(PSRAM_SPI_PIN_CK, 1); \
+        asm("nop");                    \
+        gpio_put(PSRAM_SPI_PIN_CK, 0); \
+    }
+
+void spi_tx_array(const uint8_t *data, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        uint8_t byte = data[i];
+        for (int j = 7; j >= 0; j--)
+        {
+            spi_set_mosi((byte >> j) & 0x01);
+            spi_pulse_sck();
+        }
+    }
+}
+
+void spi_rx_array(uint8_t *data, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        uint8_t byte = 0;
+        for (int j = 7; j >= 0; j--)
+        {
+            spi_pulse_sck();
+            byte |= (spi_read_miso() << j);
+        }
+        data[i] = byte;
+    }
+}
 
 void sendPsramCommand(uint8_t cmd, uint chip)
 {
     if (chip)
         selectPsramChip(chip);
-    pio_spi_write8_blocking(&pioSpi, &cmd, 1);
+    spi_tx_array(&cmd, 1);
 
     if (chip)
         deSelectPsramChip(chip);
@@ -48,31 +78,30 @@ void psramReadID(uint chip, uint8_t *dst)
 {
     selectPsramChip(chip);
     sendPsramCommand(PSRAM_CMD_READ_ID, 0);
-    pio_spi_write8_blocking(&pioSpi, dst, 3);
-    pio_spi_read8_blocking(&pioSpi, dst, 6);
+    spi_tx_array(dst, 3);
+    spi_rx_array(dst, 6);
     deSelectPsramChip(chip);
 }
 
 int initPSRAM()
 {
     gpio_init(PSRAM_SPI_PIN_S1);
-    gpio_set_dir(PSRAM_SPI_PIN_S1, true);
     gpio_init(PSRAM_SPI_PIN_S2);
-    gpio_set_dir(PSRAM_SPI_PIN_S2, true);
+    gpio_init(PSRAM_SPI_PIN_TX);
+    gpio_init(PSRAM_SPI_PIN_RX);
+    gpio_init(PSRAM_SPI_PIN_CK);
+
+    gpio_set_dir(PSRAM_SPI_PIN_S1, GPIO_OUT);
+    gpio_set_dir(PSRAM_SPI_PIN_S2, GPIO_OUT);
+    gpio_set_dir(PSRAM_SPI_PIN_TX, GPIO_OUT);
+    gpio_set_dir(PSRAM_SPI_PIN_RX, GPIO_IN);
+    gpio_set_dir(PSRAM_SPI_PIN_CK, GPIO_OUT);
 
     deSelectPsramChip(PSRAM_SPI_PIN_S1);
     deSelectPsramChip(PSRAM_SPI_PIN_S2);
 
-    uint offset = pio_add_program(pioSpi.pio, &spi_cpha0_program);
-
-    pio_spi_init(pioSpi.pio, pioSpi.sm, offset,
-                 8,     // 8 bits per SPI frame
-                 2.5,   // 40 MHz @ 400 clk_sys
-                 false, // CPHA = 0
-                 false, // CPOL = 0
-                 PSRAM_SPI_PIN_CK,
-                 PSRAM_SPI_PIN_TX,
-                 PSRAM_SPI_PIN_RX);
+    gpio_put(PSRAM_SPI_PIN_TX, 0);
+    gpio_put(PSRAM_SPI_PIN_CK, 0);
 
     sleep_ms(10);
 
@@ -88,7 +117,7 @@ int initPSRAM()
     psramReadID(PSRAM_SPI_PIN_S2, chipId);
     if (chipId[1] != PSRAM_KGD)
         return 2;
-        
+
     return 0;
 }
 
@@ -119,12 +148,12 @@ void accessPSRAM(uint32_t addr, size_t size, bool write, void *bufP)
     cmdAddr[3] = addr & 0xff;
 
     selectPsramChip(ramchip);
-    pio_spi_write8_blocking(&pioSpi, cmdAddr, cmdSize);
+    spi_tx_array(cmdAddr, cmdSize);
 
     if (write)
-        pio_spi_write8_blocking(&pioSpi, b, size);
+        spi_tx_array(b, size);
     else
-        pio_spi_read8_blocking(&pioSpi, b, size);
+        spi_rx_array(b, size);
 
     deSelectPsramChip(ramchip);
 }
